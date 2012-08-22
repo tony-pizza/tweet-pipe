5cd79e6876526e43265482d1a697daa71093ea05
Ian Pearce <peeinears@gmail.com>
2012-08-22 03:45:36 -0700

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-tweet-pipe
+TweetPipe
 ==========
 
 Connect to Twitter's Streaming API via Node.js Streams (using EventStreams)
+
+## :)
+
+ - Uses [Node Streams](http://nodejs.org/api/stream.html)
+ - With the help of [EventStream](https://github.com/dominictarr/event-stream)
+ - and [Request](https://github.com/mikeal/request) (which also uses EventStream)
+ - This means you can `pipe()` stuff around, which is really great
+ - Can pull down and deflate gzipped data (and does so by default)
+ - Inspiration (and some borrowed logic) from [ntwitter](https://github.com/AvianFlu/ntwitter)
+
+## :(
+
+ - No tests
+ - Site streams currently unsupported
+ - Lacking in error handling
+
+## Put some tweets in your pipe and do stuff with them
+
+*Example:* Track the popularity of various Mexican cuisine for one minute
+
+``` js
+
+var TweetPipe = require('./index.js')
+  , es = require('event-stream');
+
+var oauth = {
+  consumer_key: 'yourconsumerkey',
+  consumer_secret: 'yourconsumersecret',
+  token: 'youraccesstoken',
+  token_secret: 'youraccesstokensecret'
+};
+
+var tp = new TweetPipe(oauth);
+
+var tacos = burritos = enchiladas = 0;
+
+var params = { track: ['taco', 'burrito', 'enchilada'] };
+tp.stream('statuses/filter', params, function (stream) {
+
+  stream.on('tweet', function (tweet) {
+    if (tweet.text.search(/\btacos?\b/i) >= 0) tacos++;
+    if (tweet.text.search(/\bburritos?\b/i) >= 0) burritos++;
+    if (tweet.text.search(/\benchiladas?\b/i) >= 0) enchiladas++;
+
+    // choose what gets piped to next stream (if anything)
+    // in this case, pipe out tweet text
+    stream.emit('data', tweet.text + '\n');
+  });
+
+  stream.on('error', function (error) {
+    console.log('Uh oh: ' + error);
+  });
+
+  stream.on('end', function () {
+    console.log("\n");
+    console.log("THE RESULTS");
+    console.log("===========");
+    console.log('Tacos: ' + tacos);
+    console.log('Burritos: ' + burritos);
+    console.log('Enchiladas: ' + enchiladas);
+  });
+
+  // stop the stream after 60 seconds
+  setTimeout(function () { stream.end(); }, 60*1000);
+}).pipe(process.stdout); // tweet text piped to stdout
+
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,154 @@
+var es = require('event-stream')
+  , util = require('util')
+  , request = require('request')
+  , zlib = require('zlib')
+  ;
+
+function merge (defaults) {
+  for (var i = 1; i < arguments.length; i++) {
+    for (var opt in arguments[i]) {
+      defaults[opt] = arguments[i][opt];
+    }
+  }
+  return defaults;
+};
+
+function TweetPipe (oauth, options) {
+
+  // oauth should look like:
+  // { consumer_key: null,
+  //   consumer_secret: null,
+  //   token: null,
+  //   token_secret: null }
+
+  var defaults = {
+    stream_base: 'https://stream.twitter.com/1',
+    user_stream_base: 'https://userstream.twitter.com/2',
+    site_stream_base: 'https://sitestream.twitter.com/2b',
+    gzip: true, // use twitter's gzipped stream
+    headers: {
+      'Accept': '*/*',
+      'User-Agent': 'peeinears/tweet-streamer'
+    }
+  };
+
+  this.options = merge(defaults, options);
+  this.options.oauth = oauth;
+  if (this.options.gzip) {
+    this.options.headers['Accept-Encoding'] = 'deflate, gzip';
+  } else {
+    this.options.headers['Connection'] = 'close';
+  }
+
+}
+
+TweetPipe.prototype.stream = function (method, params, callback) {
+  if (typeof params === 'function') {
+    callback = params;
+    params = null;
+  }
+
+  // Iterate on params properties, if any property is an array, convert it to comma-delimited string
+  if (params) {
+    Object.keys(params).forEach(function (item) {
+      if (util.isArray(params[item])) {
+        params[item] = params[item].join(',');
+      }
+    });
+  }
+
+  var stream_base = this.options.stream_base;
+
+  var http_method = 'GET';
+
+  // Stream type customizations
+  switch (method) {
+    case 'user':
+      stream_base = this.options.user_stream_base;
+      break;
+    case 'site':
+      stream_base = this.options.site_stream_base;
+      break;
+    case 'statuses/filter':
+      http_method = 'POST';
+      break;
+  }
+
+  var url = stream_base + '/' + escape(method) + '.json';
+
+  var stream = es.through(function (data) {
+    // https://dev.twitter.com/docs/streaming-apis/messages
+
+    // to catch all
+    this.emit('all', data);
+
+    // Public stream
+    if (data['delete']) {
+      this.emit('delete', data['delete']);
+
+    } else if (data['limit']) {
+      this.emit('limit', data['limit']);
+
+    } else if (data['scrub_geo']) {
+      this.emit('scrub_geo', data['scrub_geo']);
+
+    } else if (data['status_withheld']) {
+      this.emit('status_withheld', data['status_withheld']);
+
+    } else if (data['user_withheld']) {
+      this.emit('user_withheld', data['user_withheld']);
+
+    // User stream
+    } else if (data['friends']) {
+      this.emit('friends', data['friends']);
+
+    } else if (data['event']) {
+      this.emit('event', data);
+
+    // TODO: support site stream messages
+
+    } else {
+      // must be a tweet, right?
+      this.emit('tweet', data);
+    }
+    return true;
+  });
+
+  var req = request({
+    url: url,
+    method: http_method,
+    oauth: this.options.oauth,
+    headers: this.options.headers,
+    form: (http_method === 'POST' ? params : false)
+  });
+
+  req.on('error', function (error) {
+    stream.emit('error', error);
+  });
+
+  req.on('response', function (response) {
+    // Any response code greater then 200 from steam API is an error
+    if (response.statusCode > 200) {
+      stream.destroy();
+      stream.emit('error', response.statusCode);
+    }
+  });
+
+  stream.on('close', function () { req.abort(); });
+
+  // allow user to catch emitted events
+  if ( typeof callback === 'function' ) callback(stream);
+
+  var pipeline = [
+    req,
+    (this.options.gzip ? zlib.createUnzip() : null),
+    es.split(),
+    es.parse(),
+    stream
+  ].filter(function (s) { return s !== null; });
+
+  return es.pipeline.apply(null, pipeline);
+
+};
+
+module.exports = TweetPipe;
